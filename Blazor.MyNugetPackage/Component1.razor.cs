b6a4ee5b2a4edefbdd8a983766ad661294b7df01
Giuseppe Iemma <g.iemma@bitforfun.com>
2021-07-16 19:12:46 +0200

    First commit

---
+++ Blazor.MyNugetPackage/Component1.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.MyNugetPackage
+{
+    public partial class Component1
+    {
+        [Inject]
+        IJSRuntime JSRuntime { get; set; }
+        protected async void ClickMe()
+        {
+            await JSRuntime.InvokeVoidAsync("showAlert", "Hi!");
+        }
+    }
+}
